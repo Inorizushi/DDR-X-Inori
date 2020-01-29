eb5514399b98d556e8251c8cd896a12d91ea7e0b
inorizushi <inorizushi@gmail.com>
2020-01-28 19:05:47 -0600

    gawrsh darn it to heck

--- Graphics/ScreenGameplay StageFrame/default.lua
+++ Graphics/ScreenGameplay StageFrame/default.lua
@@ -2,7 +2,7 @@
 return Def.Sprite{
     InitCommand=function(s)
         if IsUsingWideScreen() then
-            s:Load(THEME:GetPathG("ScreenGameplay","StageFrame/normal"))
+            s:Load(THEME:GetPathG("ScreenGameplay","StageFrame/normal 16_9"))
         else
             s:Load(THEME:GetPathG("ScreenGameplay","StageFrame/normal 4_3"))
         end
