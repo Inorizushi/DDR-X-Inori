716d54e2731394e749bcdddd79a6693ab673383f
Sunny <inorizushi@gmail.com>
2024-06-25 13:41:47 -0500

    Remove base image on Style Select

--- BGAnimations/ScreenSelectStyle decorations/default.lua
+++ BGAnimations/ScreenSelectStyle decorations/default.lua
@@ -1,10 +1,5 @@
 local t = LoadFallbackB()
 
-t[#t+1] = Def.Sprite{
-    Texture="../../bases/style single",
-    InitCommand=function(s) s:FullScreen():diffusealpha(1) end,
-}
-
 t[#t+1] = LoadActor("footer");
 
 return t;
